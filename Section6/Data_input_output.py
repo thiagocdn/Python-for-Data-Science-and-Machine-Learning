14429faf8701ef7daef0e618817653bc754318d9
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-16 19:35:29 -0300

    Data input and output

---
+++ Section6/Data_input_output.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+df = pd.read_csv('./Section6/assets/example')
+print('Our read df:\n', df, '\n')
+
+df.to_csv('./Section6/assets/my_output', index=False)
+print('Reading our saved file:\n', pd.read_csv(
+    './Section6/assets/my_output'), '\n')
+
+df_excel = pd.read_excel(
+    './Section6/assets/Excel_Sample.xlsx', sheet_name='Sheet1', index_col=0)
+print('Our read excel file:\n', df_excel, '\n')
+
+# Saving file to excel
+df.to_excel('./Section6/assets/Excel_Sample_saved.xlsx', sheet_name='Test')
+
+# data = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
+
+# print(data[0])
+# print(data[0].head())
+
+
+engine = create_engine('sqlite:///:memory:')
+
+df.to_sql('sql_output', engine)
+
+sqldf = pd.read_sql('sql_output', con=engine)
+
+print('Out sqldf:\n', sqldf, '\n')
