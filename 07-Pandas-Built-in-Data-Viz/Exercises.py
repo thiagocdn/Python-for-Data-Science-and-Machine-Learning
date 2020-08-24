458b027ed41c714faff30b053a61907f411aa20a
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-24 20:11:59 -0300

    Exercises

---
+++ 07-Pandas-Built-in-Data-Viz/Exercises.py
@@ -0,0 +1,25 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+df3 = pd.read_csv('df3')
+
+# df3.info()
+# df3.head()
+
+# df3.plot.scatter(x='a', y='b', figsize=(12,3), c='red', s=100, xlim=(-0.2,1.2), ylim=(-0.2,1.2))
+
+# df3['a'].hist()
+
+# plt.style.use('ggplot')
+# df3['a'].hist(bins=25)
+
+# df3[['a', 'b']].plot.box()
+
+# df3['d'].plot.kde()
+
+# df3['d'].plot.kde(lw=3, ls='--', color='orange')
+
+# df3.plot.area(xlim=(0,30)).legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+
+# %%
