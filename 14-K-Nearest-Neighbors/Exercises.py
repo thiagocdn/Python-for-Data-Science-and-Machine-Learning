eca25c51567868809458d6754c8a3edbc91a72a4
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-09-23 19:31:39 -0300

    KNN Project

--- 14-K-Nearest-Neighbors/Exercises.py
+++ 14-K-Nearest-Neighbors/Exercises.py
@@ -1,5 +1,7 @@
 # %%
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -28,11 +30,27 @@ y = df['TARGET CLASS']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.30)
 
-knn = KNeighborsClassifier(n_neighbors=1)
+# knn = KNeighborsClassifier(n_neighbors=1)
 
-knn.fit(X_train, y_train)
+# knn.fit(X_train, y_train)
 
-pred = knn.predict(X_test)
+# pred = knn.predict(X_test)
 
-confusion_matrix(y_test, pred)
+# confusion_matrix(y_test, pred)
+# print(classification_report(y_test, pred))
+
+error_rate = []
+
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    pred = knn.predict(X_test)
+    error_rate.append(np.mean(pred != y_test))
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, 40), error_rate, color='blue', linestyle='dashed',
+         marker='o', markerfacecolor='red', markersize=10)
+
+
 # %%
